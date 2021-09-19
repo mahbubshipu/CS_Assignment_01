fb82b78b57f2ff8de057e5e921e2103b625bf672
Md. Mahbubul Alom Shipu <shipumdmahbubulalom@gmail.com>
2021-09-19 10:00:09 +0600

    Task_01 done

--- Assignment_01/Exam_01/Exam_01/Program.cs
+++ Assignment_01/Exam_01/Exam_01/Program.cs
@@ -6,7 +6,30 @@ namespace Exam_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                string s1 = Console.ReadLine();
+                if (s1 == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    //int sum = 0;
+                    //for (int i = 0; i < s1.Length; i++)
+                    //{
+                    //    char c = s1[i];
+                    //    sum += (int)c;
+                    //}
+                    var sum = 0;
+                    foreach (var ch in s1)
+                    {
+                        sum += (int)ch;
+                    }
+
+                    Console.WriteLine(sum);
+                }
+            }
         }
     }
 }
